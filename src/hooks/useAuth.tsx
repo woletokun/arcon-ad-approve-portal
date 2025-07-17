81a65aeb065766870bb3e33497e4465f1b2ad328
woletokun <85924936+woletokun@users.noreply.github.com>
2025-07-17 18:17:37 +0100

    Update useAuth.tsx

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,3 +1,4 @@
+// useAuth.tsx
 import { createContext, useContext, useEffect, useState } from "react";
 import { supabase } from "@/lib/supabase";
 
@@ -5,28 +6,52 @@ const AuthContext = createContext<any>(null);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [session, setSession] = useState<any>(null);
+  const [user, setUser] = useState<any>(null);
 
   useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
+    const setup = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
       setSession(session);
-    });
 
-    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-
-    return () => {
-      listener.subscription.unsubscribe();
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      setUser(user);
     };
+
+    setup();
+
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      async (_event, session) => {
+        setSession(session);
+        const { data } = await supabase.auth.getUser();
+        setUser(data.user);
+      }
+    );
+
+    return () => listener?.subscription.unsubscribe();
   }, []);
 
-  return (
-    <AuthContext.Provider value={{ session }}>
-      {children}
-    </AuthContext.Provider>
-  );
+  const signIn = (email: string, password: string) =>
+    supabase.auth.signInWithPassword({ email, password });
+
+  const signUp = (email: string, password: string, metadata: any) =>
+    supabase.auth.signUp({
+      email,
+      password,
+      options: { data: metadata },
+    });
+
+  const value = {
+    user,
+    session,
+    signIn,
+    signUp,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
-export const useAuth = () => {
-  return useContext(AuthContext);
-};
+export const useAuth = () => useContext(AuthContext);
